d07d65b979f38038ddc047f9420956eaabdcda8e
janerz6 <je1468@student.uni-lj.si>
2016-04-06 10:38:49 +0200

    Stara konfiguracija na novem branchu, urejen handlebars in navigacija

--- js/app.js
+++ js/app.js
@@ -1,3 +1,65 @@
 /**
  * Created by Janez on 31.3.2016.
  */
+
+//Function that loads templates
+Handlebars.getTemplate = function(name) {
+    if (Handlebars.templates === undefined || Handlebars.templates[name] === undefined) {
+        $.ajax({
+            url : './templates/' + name + '.handlebars',
+            success : function(data) {
+                if (Handlebars.templates === undefined) {
+                    Handlebars.templates = {};
+                }
+                Handlebars.templates[name] = Handlebars.compile(data);
+            },
+            async : false
+        });
+    }
+    return Handlebars.templates[name];
+};
+
+//Basic routing
+$(function(){
+
+    // Bind the event.
+    $(window).on('hashchange',hashchanged);
+
+    // Trigger the event (useful on page load).
+    hashchanged();
+
+});
+function getParameter(p) {
+// returns NULL if the parameter p is not found
+    var re = new RegExp('&'+p+'=([^&]*)','i');
+    // for testing,
+    //   replace with window.location.search in real life!
+    var c = window.location.search;
+    console.log(c);
+    return (c=c.replace(/^\?/,'&').match(re)) ?c=c[1] :c='NULL';
+};
+
+function hashchanged(){
+    var search = null;
+    if(window.location.hash === '')
+        hash = 'fullscreen';
+    else {
+        var hash = location.hash.replace(/^#/, '');
+        search = getParameter('search');
+
+    }
+    var testCompiledTemplate = Handlebars.getTemplate(hash);
+
+    //Render the data into the template
+    var testRenderedTemplate = testCompiledTemplate({city: "Ljubljana", number: "3",street:"Prešernova ulica"});
+
+    //Overwrite the contents of #target with the renderer HTML
+    document.getElementById('app-content').innerHTML = testRenderedTemplate;
+    if(search !== null)
+        document.getElementById(search).scrollIntoView();
+}
+
+$(document).ready(function(){
+    //$.material.init();
+
+});
