bae99a91949808c98506cdc0cbac3843415b9dc3
janerz6 <je1468@student.uni-lj.si>
2016-04-10 22:32:27 +0200

    Update style-a za osnovno galerijo in dodani podatki v data file

--- data/data.js
+++ data/data.js
@@ -5,23 +5,89 @@
 var data = {
     images: [
         {
-            fileName:'Labod',
-            fileExt:'png'
+            fileName: 'Labod',
+            fileExt: 'png',
+            title:`Miran Erič
+            Leda z labodom / Leda and the Swan
+            1988
+            `,
+            sizeAndTechnique:`410 x 277 cm:  4 deli / 4 pieces
+            Platno, akril / Canvas, acrylic
+            `,
+            descriptionSi:`Vsem slikam iz tega časa so enotna izhodišča, ki so me takrat vznemirjala:
+            Raziskave kaosa v 19. stoletju, fraktalna geometrija, smisel mandal in paradoks, ki ga je v slikarstvu najbolj ekspicitno prikazal M.C. Esher …
+            `,
+            descriptionEn: `All paintings from that period have common starting points that excited me at that time:
+            Chaos researching in 19. century, fractal geometry, the meaning of mandalas and the paradox that was in painting most explicitly demonstrated by M. C. Esher …
+            `
+
         }
         ,
         {
-            fileName:'pismenke',
-            fileExt:'png'
+            fileName: 'pismenke',
+            fileExt: 'png',
+            title:` Miran Erič
+            Svet / World
+            1988
+            `,
+            sizeAndTechnique:`400 x 300 cm (preveriti): 12 delov / 12 pieces
+            Olje, steklo, plastika, plastelin / Oil, glass, plastic, plasticine
+            `,
+            descriptionSi:'Pred nastankom slike sem skoraj dva meseca potoval po Kitajski. ' +
+            'Poln vtisov in spoznanja o naši majhnosti in "majhnosti" v naših glavah, sem naslikal Svet.',
+            descriptionEn: 'Just before I created this painting I traveled two months in China. ' +
+            'Full of impressions and awareness of our small size and “smallness” in our heads, I painted the World.'
         }
         ,
         {
-            fileName:'RibaRaca',
-            fileExt:'png'
+            fileName: 'RibaRaca',
+            fileExt: 'png',
+            title:` Miran Erič
+            Riba reže raci rep / Fish cuts duck’s tail
+            1987
+            `,
+            sizeAndTechnique:`351 x 301 cm: 25 delov / 25 pieces
+            Iverna plošča, olje / Particle board, oil
+            `,
+            descriptionSi:` Čas v katerem so slike nastajale je bi turbolenten in osvobajajoč.
+            Izjemna pozitivna energija, ki je v 80’ naplavila množico alternativnih gibanj,
+            močan razcvet umetniških dejavnosti in preizpraševanje o družbi in njenih prepovedanih temah,
+            kakršne so nasilje nad ženskami, homoseksualnost, izkoriščanje marginalnih skupin,
+            kot so delavci iz juga ali cigani … Bil sem del tega.
+            `,
+            descriptionEn: `The times when my pictures were painted were turbulent and liberating.
+            The exceptional positive energy that set in motion a multitude of alternative movements,
+            flourishing of artistic activities and questioning the society and its forbidden issues,
+            such as violence towards women, homosexuality, exploitation of marginal groups such as migrant
+            workers from the south or gypsies … I was part of all that.
+            `
         }
         ,
         {
-            fileName:'Zirafa',
-            fileExt:'png'
+            fileName: 'Zirafa',
+            fileExt: 'png',
+            title:`Miran Erič
+            Žirafa / Giraffe 
+            1991
+            `,
+            sizeAndTechnique:`200 x 400 cm: 3 deli / 3 pieces
+            Platno, kolaž, akril: organski materiali, tkanina, pesek, lepilo / Canvas, collage, acrylic: organic materials, fabric, sand, glue
+            `,
+            descriptionSi:`Prav proti koncu slikanja, ko sem še imel na razpolago atelje, sem domislil serijo slik, ki bi upodabljale živali:
+             žirafa je bila prva od teh. Ideja serije pa je bila, da bi bile slike živali v merilu približno 1:1.
+             Samice žiraf zrastejo do višine okoli 4 - 5 m. Samci pa tudi do 6 m.
+             Slika tako predstavlja mlado žirafo …
+             Slika miške bi tako bila velika okoli 3 x 5 cm, hrošča pa še manjša …
+             Seveda sem želel naslikat tudi sinjega kita …
+            `,
+            descriptionEn: `Towards the end of my painting period, when I still had a studio at my disposal,
+            I envisioned a series of paintings of animals: giraffe was the first of these.
+            The idea of the series was that all paintings would depict animals in scale of 1:1.
+            Giraffe females grow up to a height of 4 - 5 m. Males even up to 6 m. The painting depicts a young giraffe …
+            A painting of a mouse would be of size around 3 x 5 cm, a painting of a beetle even smaller …
+            Of course I wanted to painted also a blue whale …
+            `
         }
     ]
-};
+}
+;
