51920f2f77436bbfa69ada6fc5232c3c57f249ba
Janez Erzen <je1468@student.uni-lj.si>
2016-04-20 00:28:28 +0200

    Delo na glavni strani galerije

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,11 +1,46 @@
 /**
  * Created by Janez on 31.3.2016.
  */
+function moveBtns() {
+    var left = $("#image").position().left;
+    var right = $("#image").position().left + $("#image").width();
+    var top = $("#image").position().top;
+    var height = $("#image").height();
+    var btnHeight = $("#prevImgBtn").height();
+    var btnWidth = $("#prevImgBtn").width();
+    //console.log(left + " " + right);
+    var h = top + height / 2 - btnHeight / 2;
+    $("#prevImgBtn").css({left: left - btnWidth, position: 'absolute !important', top: h});
+    $("#nextImgBtn").css({left: right, position: 'absolute !important', top: h});
+}
 
-function registerEvents(){
+function toggleFullscreen(tm) {
+    var time = tm || 1200;
+    if (!fullscreenMode) {
+        $("#imgContainer").animate({height: '95vh'}, time);
+        //$("#prevImgBtn").animate({position:'fixed !important',top:'50%',left:'2%'},1500);
+        //$("#nextImgBtn").animate({position:'fixed !important',top:'50%',right:'2%'},1500);
+    }
+    else {
+        $("#imgContainer").animate({height: '65vh'}, time);
+    }
+    fullscreenMode = !fullscreenMode;
+    $("#viewsMenu").slideToggle(1500);
+    $(".galleryBasicImgDescription").slideToggle(time);
 
+    var count = 0;
+    var id = setInterval(function () {
+        moveBtns();
+        if (count === time / 10) {
+            clearInterval(id);
+        }
+        count++
+    }, 10);
+}
+function registerEvents() {
+    setTimeout(moveBtns, 200);
     // about author click
-    $("#detail").on('click',function(){
+    $("#detail").on('click', function () {
         $("#bs-body").show(500);
         $("#bs-tail").hide(500);
         $("#main").animate({
@@ -13,7 +48,7 @@ function registerEvents(){
         }, 2000);
     });
 
-    $("#hide").on('click',function(){
+    $("#hide").on('click', function () {
         $("#bs-body").hide(200);
         $("#bs-tail").show(500);
         $("#main").animate({
@@ -22,13 +57,13 @@ function registerEvents(){
     });
 
     /* basic gallery page */
-    $("#toggleViewType").on('click',function(){
+    $("#toggleViewType").on('click', function () {
         $("#viewsMenu").slideToggle();
     });
 
-     /* Basic gallery */
-    $("#toggleZoom").on('click',function(){
-        if(!innerZoomActive) {
+    /* Basic gallery */
+    $("#toggleZoom").on('click', function () {
+        if (!innerZoomActive) {
             innerZoomActive = true;
             $('#image').elevateZoom(innerZoomConfig);
         }
@@ -36,47 +71,47 @@ function registerEvents(){
             innerZoomActive = false;
             $.removeData($("#image"), 'elevateZoom');
         }
-        console.log('innerZoomActive:',innerZoomActive);
+        console.log('innerZoomActive:', innerZoomActive);
     });
 
-    $("#nextImgBtn").on('click',function(e){
+    $("#nextImgBtn").on('click', function (e) {
         e.preventDefault();
-        selectedImageIdx = (selectedImageIdx + 1) % (data.images.length);
-        //Trigger redraw
-        $(window).trigger("hashchange");
+        if (fullscreenMode) {
+            fullscreenMode = !fullscreenMode;
+            selectedImageIdx = (selectedImageIdx + 1) % (data.images.length);
+            //Trigger redraw
+            $(window).trigger("hashchange");
+            toggleFullscreen(2);
+
+        }
+        else{
+            selectedImageIdx = (selectedImageIdx + 1) % (data.images.length);
+            //Trigger redraw
+            $(window).trigger("hashchange");
+        }
     });
-    $("#prevImgBtn").on('click',function(e){
+    $("#prevImgBtn").on('click', function (e) {
         e.preventDefault();
         selectedImageIdx = (selectedImageIdx - 1)
-        if(selectedImageIdx === 0)
+        if (selectedImageIdx === 0)
             selectedImageIdx = (data.images.length - 1);
         //Trigger redraw
         $(window).trigger("hashchange");
     });
 
-    $("#fullscreen, #image").on('click',function(){
-
-
-        if(!fullscreenMode){
-            $("#imgContainer").animate({height:'90vh'},1500);
-            //$("#prevImgBtn").animate({position:'fixed !important',top:'50%',left:'2%'},1500);
-            //$("#nextImgBtn").animate({position:'fixed !important',top:'50%',right:'2%'},1500);
-        }
-        else{
-            $("#imgContainer").animate({height:'65vh'},1500);
-        }
-
-        fullscreenMode = !fullscreenMode;
-        $("#viewsMenu").slideToggle(1500);
-        $(".galleryBasicImgDescription").slideToggle(1500);
+    $("#fullscreen, #image").on('click', function () {
+        toggleFullscreen();
     });
+
+    $(window).resize(moveBtns);
+
     /* Interview */
-    $('.interviewPage .indexContainer a').on('click',function(e){
+    $('.interviewPage .indexContainer a').on('click', function (e) {
         e.preventDefault();
         $(".indexContainer ul li").removeClass("active");
         $(this).parent().addClass("active");
 
         window.scrollTo(0, $($(this).attr("href")).position().top);
     });
-   
+
 }
