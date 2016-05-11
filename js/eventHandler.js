4a8c0ce9371157ccde7137e3b7f83ae62c1baf29
Benjamin <benjaminovak@gmail.com>
2016-05-11 14:55:05 +0200

    Odzivnost intervjuja na velikost okna

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -3,45 +3,61 @@
  */
 
 function registerEvents() {
-    setTimeout(moveBtns, 500);
-    // about author click
-    $("#detail").on('click', function () {
-        $("#bs-body").show(500);
-        $("#bs-tail").hide(500);
-        $("#main").animate({
-            paddingTop: "5%"
-        }, 2000);
-    });
-
-    $("#hide").on('click', function () {
-        $("#bs-body").hide(200);
-        $("#bs-tail").show(500);
-        $("#main").animate({
-            paddingTop: "15%"
-        }, 2000);
-    });
-
-    basicGalleryEvents();
-    doubleGalleryEvents();
-
-    /* Interview */
-    $('.interviewPage .indexContainer a').on('click',function(e){
-        e.preventDefault();
-        $(".indexContainer ul li").removeClass("active");
-        $(this).parent().addClass("active");
-        window.scrollTo(0, $($(this).attr("href")).position().top - 10);
-    });
-
-    $("#back").on('click',function(){
-        window.scrollTo(0, 0);
-        window.location.replace("./");
-    });
 
 
-    $('body').scrollspy({
-        target: '.bs-docs-sidebar',
-        offset: 200
-    });
 
+    if(window.location.href.split("#")[1] === "galleryBasic") {
+        basicGalleryEvents();
+    }
+    else if(window.location.href.split("#")[1] === "galleryDouble") {
+        doubleGalleryEvents();
+    }
+    else{
+
+        setTimeout(moveBtns, 500);
+        // about author click
+        $("#detail").on('click', function () {
+            $("#bs-body").show(500);
+            $("#bs-tail").hide(500);
+            $("#main").animate({
+                paddingTop: "5%"
+            }, 2000);
+        });
+
+        $("#hide").on('click', function () {
+            $("#bs-body").hide(200);
+            $("#bs-tail").show(500);
+            $("#main").animate({
+                paddingTop: "15%"
+            }, 2000);
+        });
+
+        $("#back").on('click',function(){
+            window.scrollTo(0, 0);
+            window.location.replace("./");
+        });
+
+        $('body').scrollspy({
+            target: '.bs-docs-sidebar',
+            offset: 250
+        });
+
+        if($( window ).width() < 1024) {
+            $( ".bback" ).hide();
+            $( "div.wback" ).attr('class',  "col-md-12 col-sm-12 wback" );
+        } else {
+            $( ".bback" ).show();
+            $( "div.wback" ).attr('class',  "col-md-6 col-sm-6 wback" );
+        }
+        $( window ).resize(function() {
+            if($( window ).width() < 1024) {
+                $( ".bback" ).hide();
+                $( "div.wback" ).attr('class',  "col-md-12 col-sm-12 wback" );
+            } else {
+                $( ".bback" ).show();
+                $( "div.wback" ).attr('class',  "col-md-6 col-sm-6 wback" );
+            }
+        });
+    }
 
 }
